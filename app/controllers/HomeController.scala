3ff121b9dfe4d35759a4a9fc0ce526315ecfd4ac
Alexey <alexey.rykhalskiy@gmail.com>
2020-01-24 23:25:16 +0200

    -- fix

---
+++ app/controllers/HomeController.scala
@@ -0,0 +1,16 @@
+package controllers
+
+import javax.inject._
+import play.api.mvc._
+
+@Singleton
+class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+
+  def index() = Action { implicit request: Request[AnyContent] =>
+    import Console._
+    val hh: Seq[(String, String)] = request.headers.headers
+    hh.map(h => s"${BLUE}${h._1}$RESET:$RED${h._2}$RESET").foreach(println)
+    Ok("Minimal Play App is working!")
+  }
+
+}
--- build.sbt
+++ build.sbt
@@ -4,13 +4,8 @@ version := "1.0-SNAPSHOT"
 
 lazy val root = (project in file(".")).enablePlugins(PlayScala)
 
-scalaVersion := "2.11.7"
+scalaVersion := "2.13.1"
 
 libraryDependencies ++= Seq(
-  jdbc,
-  cache,
-  "org.postgresql" % "postgresql" % "9.4-1201-jdbc41",
-  ws
+  guice,
 )
-
-libraryDependencies <+= scalaVersion("org.scala-lang" % "scala-compiler" % _ )
